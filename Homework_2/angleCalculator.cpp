c84370b65ccafe080dc734a3c3ef4aec07a4281a
Lucas Dahl <LucasDahl425@yahoo.com>
2020-04-16 17:22:04 -0700

    Initial commit

---
+++ Homework_2/angleCalculator.cpp
@@ -0,0 +1,19 @@
+//
+//  angleCalculator.cpp
+//  Homework_2
+//
+//  Created by Lucas Dahl on 4/16/20.
+//  Copyright © 2020 Lucas Dahl. All rights reserved.
+//
+
+//// Header
+//#include <iostream>
+//#include <string>
+//#include <iomanip>
+
+//using namespace std;
+//
+//int main() {
+//   // Used to exit program
+//    return 0;
+//}
---
+++ Homework_2/ingredientAdjuster.cpp
@@ -0,0 +1,21 @@
+//
+//  ingredientAdjuster.cpp
+//  Homework_2
+//
+//  Created by Lucas Dahl on 4/16/20.
+//  Copyright © 2020 Lucas Dahl. All rights reserved.
+//
+
+// Header
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    
+    
+   // Used to exit program
+    return 0;
+}
