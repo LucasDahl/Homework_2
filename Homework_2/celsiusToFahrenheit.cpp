87ac200f0bd97814030ecb81aa8815c8dd412c58
Lucas Dahl <LucasDahl425@yahoo.com>
2020-04-17 11:06:33 -0700

    Finished the Celsius to Fahrenheit conversion assignment.

--- Homework_2/celsiusToFahrenheit.cpp
+++ Homework_2/celsiusToFahrenheit.cpp
@@ -8,12 +8,35 @@
 
 // Header
 #include <iostream>
-#include <string>
-#include <iomanip>
 
 using namespace std;
 
+int tempConvert(float temp) {
+    
+    // temp properties
+    float f;
+    
+    // Make the conversion
+    f = ((9/5) * temp + 32);
+    
+    return f;
+}
+
 int main() {
+    
+    // Properties
+    float celsius, fahrenheit;
+    
+    // Ask the user to input the temp in celsius that they want to convert
+    cout << "Please enter the Celsius temperature you want to convert to Fahrenheit: ";
+    cin >> celsius;
+    
+    // Call the conversion method and assign it to fahrenheit
+    fahrenheit = tempConvert(celsius);
+    
+    // Display the message and set the precision
+    cout << "If the Celsius temperature is " << celsius << "degrees," << endl;
+    cout << "Then it is " << fahrenheit << " degrees Farenheit." << endl;
     
     
    // Used to exit program
